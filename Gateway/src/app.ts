a3afca5595542313b22e36b98b03da4b306635c7
FerchoXD <213524@ids.upchiapas.edu.mx>
2024-12-08 03:41:53 -0600

    gateway completo

--- Gateway/src/app.ts
+++ Gateway/src/app.ts
@@ -18,8 +18,11 @@ const SERVICES = {
     AUTH: process.env.AUTH_URL || 'http://127.0.0.1:3001',
     HISTORY: process.env.HISTORY_URL || 'http://127.0.0.1:3002',
     FORUM: process.env.FORUM_URL || 'http://127.0.0.1:3003',
-    //CHAT: process.env.CHAT_URL || 'http://127.0.0.1:3004',
-    PAYMENT: process.env.PAYMENT_URL || 'http://127.0.0.1:3004',
+    NOTIFICATIONS: process.env.NOTIFICATIONS_URL || 'http://127.0.0.1:3004',
+    PAYMENT: process.env.PAYMENT_URL || 'http://127.0.0.1:3005',
+    FINANCE: process.env.FINANCE_URL || 'http://127.0.0.1:3006',
+    VIRTUALASSISTANT: process.env.VIRTUALASSISTANT_URL || 'http://127.0.0.1:3007',
+    //CHAT: process.env.CHAT_URL || 'http://127.0.0.1:4000',
     RAITING: process.env.RAITING_URL || 'http://127.0.0.1:5000',
 };
 
@@ -36,6 +39,9 @@ const createProxy = (target: string) =>
 app.use('/api/v1/auth', createProxy(SERVICES.AUTH));
 app.use('/api/v1/history', createProxy(SERVICES.HISTORY));
 app.use('/api/v1/forum', createProxy(SERVICES.FORUM));
+app.use('/api/v1/notifications', createProxy(SERVICES.NOTIFICATIONS));
+app.use('/api/v1/finance', createProxy(SERVICES.FINANCE));
+app.use('/api/v1/virtualassistant', createProxy(SERVICES.VIRTUALASSISTANT));
 //app.use('/api/v1/chat', createProxy(SERVICES.CHAT));
 app.use('/api/v1/payment', createProxy(SERVICES.PAYMENT));
 app.use('/api/v1/raiting', createProxy(SERVICES.RAITING));
